7b96a32d4f0fa0413de99c980705ac550fdcd0d7
MariaDaniela <maria.henrique@ccc.ufcg.edu.br>
2015-09-05 19:56:50 -0300

    Excluindo pasta que estava causando conflito

--- app/src/main/java/finddelivery/es/projeto/finddelivery/models/EmptyFieldException.java
+++
@@ -1,9 +0,0 @@
-package finddelivery.es.projeto.finddelivery.models;
-
-public class EmptyFieldException extends Exception {
-
-    public EmptyFieldException() {
-        super("Empty Field");
-
-    }
-}
